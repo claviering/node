f9c9bb9f33440eb0fbd14505acdc9e1d4021592d
claviering <claviering@gmail.com>
2019-01-31 16:35:51 +0800

    fix

--- readXlsx.js
+++ readXlsx.js
@@ -1,12 +1,8 @@
 const XLSX = require('xlsx')
-// const fs = require('fs')
-// let stream = fs.createWriteStream("data.txt")
-
-let filePath = './try.xlsx' // 文件路径
-let SheetHead = 'sheet0' // 表名称
+const filePath = './try.xlsx' // 文件路径
+const SheetHead = 'sheet0' // 表名称
 
 console.log('loading...')
-
 let workbook = XLSX.readFile(filePath)
 console.log(workbook.Strings) // 获取表头
 let dataList = XLSX.utils.sheet_to_json(workbook.Sheets[SheetHead])
